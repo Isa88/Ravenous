8cd514175dc7e377a1964ed4674cf5d261c91543
Isabelle <isaperarnaud@gmail.com>
2018-02-17 00:38:43 +0100

    Hello

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business';
+
+class BusinessList extends React.Component {
+  render() {
+    <div class="BusinessList">
+      <!-- Add Business Component here -->
+      <!-- Add Business Component here -->
+      <!-- Add Business Component here -->
+      <!-- Add Business Component here -->
+      <!-- Add Business Component here -->
+      <!-- Add Business Component here -->
+    </div>
+  }
+}
